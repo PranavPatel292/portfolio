14624d977f239a15b231573c2e11b627f7de95c3
Pranav Patel <42827820+PranavPatel292@users.noreply.github.com>
2020-07-25 17:26:49 +1000

    Updates

---
+++ precache-manifest.58a85e19c06f2be45fee77036dbaac66.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c226ee440b4555e0d8fa27db5b7c9ff6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b79c418a3fc9f9d531d0",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "44258da040e05c2a3737",
+    "url": "/portfolio/static/css/main.52f10468.chunk.css"
+  },
+  {
+    "revision": "b79c418a3fc9f9d531d0",
+    "url": "/portfolio/static/js/2.2487ec8e.chunk.js"
+  },
+  {
+    "revision": "c0a7484cc31358a0df86ce48ef08f02b",
+    "url": "/portfolio/static/js/2.2487ec8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44258da040e05c2a3737",
+    "url": "/portfolio/static/js/main.10f34d14.chunk.js"
+  },
+  {
+    "revision": "52083269804884c31be5",
+    "url": "/portfolio/static/js/runtime-main.7099e02f.js"
+  },
+  {
+    "revision": "59f95faf9afb8e2e323620ed75f0851b",
+    "url": "/portfolio/static/media/Pranav_Resume.59f95faf.pdf"
+  },
+  {
+    "revision": "bbb668b8f21ea029081596b1fed47341",
+    "url": "/portfolio/static/media/astronomy.bbb668b8.png"
+  },
+  {
+    "revision": "f34557184984b6e95dc1d6d28fcb54e6",
+    "url": "/portfolio/static/media/git.f3455718.jpg"
+  },
+  {
+    "revision": "55f044ac5422950f8731e63069abf962",
+    "url": "/portfolio/static/media/pranav.55f044ac.png"
+  },
+  {
+    "revision": "48a9f30aa124839eeee26c89307059e1",
+    "url": "/portfolio/static/media/projectFifth.48a9f30a.png"
+  },
+  {
+    "revision": "5e3f89b3f147c884629704bde4c31e77",
+    "url": "/portfolio/static/media/projectForth.5e3f89b3.png"
+  },
+  {
+    "revision": "1c72ea66ddf9e1f5791c1785de2b1496",
+    "url": "/portfolio/static/media/projectSecond.1c72ea66.png"
+  },
+  {
+    "revision": "fa280fb1473b0d4cf41fc59e02435865",
+    "url": "/portfolio/static/media/projectSix.fa280fb1.png"
+  },
+  {
+    "revision": "2b2ffcc8d7784671212b239f245a58db",
+    "url": "/portfolio/static/media/projectThird.2b2ffcc8.png"
+  },
+  {
+    "revision": "99fa9647a7026749d4ed29fb2b98c3ec",
+    "url": "/portfolio/static/media/third.99fa9647.jpg"
+  },
+  {
+    "revision": "22402260f8ac2002686efb88ef23ed6d",
+    "url": "/portfolio/static/media/traveling.22402260.png"
+  }
+]);
