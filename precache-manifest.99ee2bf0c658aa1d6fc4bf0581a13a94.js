56b171bf3a8089343c3c3d619c5867c7499a15d7
Pranav Patel <42827820+PranavPatel292@users.noreply.github.com>
2020-07-25 17:09:30 +1000

    Updates

---
+++ precache-manifest.99ee2bf0c658aa1d6fc4bf0581a13a94.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f1df5458ac4cadc216849586b66df8",
+    "url": "/PranavPatel292/portfolio.git/index.html"
+  },
+  {
+    "revision": "b79c418a3fc9f9d531d0",
+    "url": "/PranavPatel292/portfolio.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a338c8c27ecb799c6d55",
+    "url": "/PranavPatel292/portfolio.git/static/css/main.aa027278.chunk.css"
+  },
+  {
+    "revision": "b79c418a3fc9f9d531d0",
+    "url": "/PranavPatel292/portfolio.git/static/js/2.2487ec8e.chunk.js"
+  },
+  {
+    "revision": "c0a7484cc31358a0df86ce48ef08f02b",
+    "url": "/PranavPatel292/portfolio.git/static/js/2.2487ec8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a338c8c27ecb799c6d55",
+    "url": "/PranavPatel292/portfolio.git/static/js/main.10f34d14.chunk.js"
+  },
+  {
+    "revision": "8fdc2bce985a1e30d12b",
+    "url": "/PranavPatel292/portfolio.git/static/js/runtime-main.31b47982.js"
+  },
+  {
+    "revision": "59f95faf9afb8e2e323620ed75f0851b",
+    "url": "/PranavPatel292/portfolio.git/static/media/Pranav_Resume.59f95faf.pdf"
+  },
+  {
+    "revision": "bbb668b8f21ea029081596b1fed47341",
+    "url": "/PranavPatel292/portfolio.git/static/media/astronomy.bbb668b8.png"
+  },
+  {
+    "revision": "f34557184984b6e95dc1d6d28fcb54e6",
+    "url": "/PranavPatel292/portfolio.git/static/media/git.f3455718.jpg"
+  },
+  {
+    "revision": "55f044ac5422950f8731e63069abf962",
+    "url": "/PranavPatel292/portfolio.git/static/media/pranav.55f044ac.png"
+  },
+  {
+    "revision": "48a9f30aa124839eeee26c89307059e1",
+    "url": "/PranavPatel292/portfolio.git/static/media/projectFifth.48a9f30a.png"
+  },
+  {
+    "revision": "5e3f89b3f147c884629704bde4c31e77",
+    "url": "/PranavPatel292/portfolio.git/static/media/projectForth.5e3f89b3.png"
+  },
+  {
+    "revision": "1c72ea66ddf9e1f5791c1785de2b1496",
+    "url": "/PranavPatel292/portfolio.git/static/media/projectSecond.1c72ea66.png"
+  },
+  {
+    "revision": "fa280fb1473b0d4cf41fc59e02435865",
+    "url": "/PranavPatel292/portfolio.git/static/media/projectSix.fa280fb1.png"
+  },
+  {
+    "revision": "2b2ffcc8d7784671212b239f245a58db",
+    "url": "/PranavPatel292/portfolio.git/static/media/projectThird.2b2ffcc8.png"
+  },
+  {
+    "revision": "99fa9647a7026749d4ed29fb2b98c3ec",
+    "url": "/PranavPatel292/portfolio.git/static/media/third.99fa9647.jpg"
+  },
+  {
+    "revision": "22402260f8ac2002686efb88ef23ed6d",
+    "url": "/PranavPatel292/portfolio.git/static/media/traveling.22402260.png"
+  }
+]);
